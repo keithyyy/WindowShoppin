30a577e31c359a517ee685022567d68bc448aeb7
Keith Crooc <keith.crooc@gmail.com>
2021-01-23 13:21:11 -0500

    uncommented dp.seqeulize

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Syncing our database and logging a message to the user upon success
-//db.sequelize.sync().then(function () {
+db.sequelize.sync().then(function () {
 app.listen(PORT, function () {
   console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });
-//});
+});
 
 

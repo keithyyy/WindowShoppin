576598762642f81e99fed8ae9ddf352917292089
Lina Zughaiyer <linazughaiyer@hotmail.com>
2021-01-23 10:52:27 -0500

    Database

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+//db.sequelize.sync().then(function () {
+app.listen(PORT, function () {
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });
+//});
 

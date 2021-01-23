4dcf94e4b3068ba7f6eea621c9b3a14df6d24221
Eldar <elogon@gmail.com>
2021-01-23 13:53:19 -0500

    Create puppeteer scraper controller

---
+++ controllers/scraper.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+async function helloWorld() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program');
+
+  const firstPar = await page.$eval('#mw-content-text p', el => el.innerText);
+
+  console.log(firstPar); // A "Hello, World!" program is a computer program that outputs ...
+
+  await browser.close();
+}

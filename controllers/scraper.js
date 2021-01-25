1a918ff9fb111ef2a1247abc36674bed53eaec86
Eldar <elogon@gmail.com>
2021-01-25 10:39:52 -0500

    Improve scraper to get info from different websites

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -22,37 +22,41 @@ async function scrapeItem(url, cb) {
 
   let itemPromise = (url) => new Promise(async(resolve, reject) => {
       try {
-          let arrOfData = [];
+        // let arrOfData = [];
         // let dataObj = {};
-        // // await page.waitForSelector('#productTitle');
+        // await page.waitForSelector('#productTitle');
         // dataObj['url'] = link;
-        dataObj['title'] = await page.$$eval('[class*="product-title"], [class*="productName"], [class*="product-name"], [class*="productTitle"]', text => text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, ""));
+        // dataObj['title'] = await page.$$eval('[class*="product-title"], [class*="productName"], [class*="product-name"], [class*="productTitle"]', text => text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, ""));
         // dataObj['initialPrice'] = await page.$eval('#priceblock_ourprice', text => Number(text.textContent.replace(/[^0-9\.]+/g,"")));
         // dataObj['description'] = await page.$eval('#productDescription', text => text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, ""));
         // // dataObj['imageUrl'] = await newPage.$eval('#ivLargeImage img', img => img.src);
+
         let newUrls = await page.evaluate(async () => {
-            // let results = {};
+            let results = {};
             // Scrape title|name of product
             let items = document.querySelectorAll('[class*="product-title"], [class*="productName"], [class*="product-name"], [class*="productTitle"]');
-            items.forEach((item) => {
+            items.forEach(item => {
                 if (item.innerText) {
-                    results = {
-                        title: item.innerText,
+                    results.title = item.innerText.trim();
                     };
-                }
-                
             });
-            let prices = document.querySelectorAll('[class*="price"], [class*="Price"]');
+            // Scrape price
+            let prices = document.querySelectorAll('[class*="price"], [class*="Price"], [class*="price__total"]');
             prices.forEach((item) => {
                 if (item.innerText) {
-                    results = {
-                        initialPrice: Number(item.innerText.replace(/[^0-9\.]+/g,""))
-                    };
+                    results.initialPrice = Number(item.innerText.replace(/[^0-9\.]+/g,""));
                 }
-                
+            });
+            // Scrape description
+            let description = document.querySelectorAll('[class*="description"], [class*="Description"], [id*="Description"] p');
+            description.forEach((item) => {
+                if (item.innerText && !results.description) {
+                    results.description = item.innerText.trim();
+                }
             });
             return results;
         });
+
         await page.close();
         newUrls.url = url;
         resolve(newUrls);

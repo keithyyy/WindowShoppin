12d4b6ec59b32469363ba9ba099b2a3d458c5ae8
Lina Zughaiyer <linazughaiyer@hotmail.com>
2021-01-24 20:03:52 -0500

    transfered a few files to proper branch

---
+++ controllers/itemsControllers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Here I'm unsure how to access the User's Items using from the models.
+//
+const db = require('../models')
+
+module.exports = function(app) {
+    
+}
---
+++ models/item.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Item.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    url: DataTypes.STRING,
+    initialPrice: DataTypes.DECIMAL(10,2),
+    newPrice: DataTypes.DECIMAL(10,2),
+    category: DataTypes.STRING,
+    retailer: DataTypes.STRING,
+    isUpdated: DataTypes.BOOLEAN,
+    imgURL: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Item',
+  });
+
+  Item.associate = (models) => {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Item.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+  return Item;
+};

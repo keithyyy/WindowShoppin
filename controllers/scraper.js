d89e0b86578a000ca4920475cb84b2763a1e4f6d
Eldar <elogon@gmail.com>
2021-01-25 14:15:54 -0500

    Improve scraper functinality

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -31,35 +31,45 @@ async function scrapeItem(url, cb) {
         // dataObj['description'] = await page.$eval('#productDescription', text => text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, ""));
         // // dataObj['imageUrl'] = await newPage.$eval('#ivLargeImage img', img => img.src);
 
-        let newUrls = await page.evaluate(async () => {
+        let product = await page.evaluate(async () => {
             let results = {};
             // Scrape title|name of product
-            let items = document.querySelectorAll('[class*="product-title"], [class*="productName"], [class*="product-name"], [class*="productTitle"]');
+            let items = document.querySelectorAll('[class*="product-title"], [class*="productName"], [class*="product-name"], [class*="productTitle"], .product-detail__title h1');
             items.forEach(item => {
                 if (item.innerText) {
                     results.title = item.innerText.trim();
                     };
             });
+            // Scrape image of product
+            let img = document.querySelectorAll('.imgTagWrapper img, [class*="productImage"], .static-product-image');
+            img.forEach(image => {
+                if (image.src) {
+                    results.imgURL = image.src;
+                };
+            });
             // Scrape price
-            let prices = document.querySelectorAll('[class*="price"], [class*="Price"], [class*="price__total"]');
+            let prices = document.querySelectorAll('[class*="price"], [class*="Price"], [class*="price__total"], .price__total-value price__total--on-sale');
             prices.forEach((item) => {
                 if (item.innerText) {
                     results.initialPrice = Number(item.innerText.replace(/[^0-9\.]+/g,""));
                 }
             });
             // Scrape description
-            let description = document.querySelectorAll('[class*="description"], [class*="Description"], [id*="Description"] p');
+            let description = document.querySelectorAll('.product-description-blurb__text, [class*="description"], [class*="Description"], [id*="Description"] p');
             description.forEach((item) => {
                 if (item.innerText && !results.description) {
-                    results.description = item.innerText.trim();
+                    results.description = item.innerText.replace(/(\r\n\t|\n|\r|\t)/gm, "");
                 }
             });
             return results;
         });
-
-        await page.close();
-        newUrls.url = url;
-        resolve(newUrls);
+        
+        // await page.close();
+        if (!product.description) {
+            product.description = product.title;
+        }
+        product.url = url;
+        resolve(product);
         
       } catch (e) {
           return reject(e)
@@ -74,7 +84,7 @@ async function scrapeItem(url, cb) {
     catch (err) {
         console.log('unable to get', err);
     }
-    await browser.close();
+    // await browser.close();
 }
 
 module.exports = scrapeItem;
